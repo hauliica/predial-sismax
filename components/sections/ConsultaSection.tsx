10738b58895c30a3bee5f71e218192a8c897bc83
Saul Ochoa <hauliica@gmail.com>
2023-12-14 06:25:40 -0600

    refactor(ConsultaSection): Update import path for Button component
    - Move the import path for the Button component from "../ui/button" to "@/components/ui/button" in the ConsultaSection.tsx file.
    - This allows for better module resolution and maintainability.
    - The Button component is used for the submit button in the ConsultaSection component.
    - The change ensures that the correct Button component is imported and used.
    - This commit improves the organization and readability of the code.

--- components/sections/ConsultaSection.tsx
+++ components/sections/ConsultaSection.tsx
@@ -3,13 +3,13 @@
 import Link from "next/link";
 import {useFormState, useFormStatus} from "react-dom";
 import {validateCuentaFolio} from "@/app/actions";
-import {Button} from "../ui/button";
+import {Button} from "@/components/ui/button";
 
 function SubmitBtn() {
     const {pending} = useFormStatus();
 
     return (
-        <Button type="submit" className="w-full sm:w-auto" disabled={pending}>
+        <Button type="submit" variant="secondary" size="lg" className="w-full py-6 sm:w-auto" disabled={pending}>
             {pending ? "Enviando..." : "Enviar"}
         </Button>
     );
@@ -23,50 +23,8 @@ export default function CTASection() {
 
     return (
         <section
-            id="pago"
             className="flex w-full items-center justify-center overflow-x-hidden py-8"
         >
-            <div
-                className="pointer-events-none relative inset-0 overflow-hidden"
-                aria-hidden="true"
-            >
-                <svg
-                    className="absolute inset-0 h-full w-full"
-                    width="100%"
-                    height="100%"
-                    fill="none"
-                    viewBox="0 0 32 32"
-                    preserveAspectRatio="none"
-                    transform="scale(3)"
-                >
-                    <path
-                        d="M16 0C7.164 0 0 7.164 0 16s7.164 16 16 16c8.837 0 16-7.163 16-16S24.837 0 16 0zm0 2c7.732 0 14 6.268 14 14S23.732 30 16 30C8.268 30 2 23.732 2 16S8.268 2 16 2z"
-                        fill="rgba(255,255,255,0.05)"
-                    />
-                </svg>
-                <svg
-                    className="absolute left-1/4 top-1/4 h-40 w-40 -translate-x-1/2 -translate-y-1/2 rotate-45 transform"
-                    viewBox="0 0 100 100"
-                    fill="rgba(255,255,255,0.10)"
-                >
-                    <rect width="100" height="100" rx="5"></rect>
-                </svg>
-                <svg
-                    className="absolute bottom-1/4 right-1/4 h-40 w-40 translate-x-1/2 translate-y-1/2 rotate-[170deg] transform"
-                    viewBox="0 0 100 100"
-                    fill="rgba(255,255,255,0.05)"
-                >
-                    <circle cx="50" cy="50" r="50"></circle>
-                </svg>
-                <svg
-                    className="bottom-6/6 absolute right-1/3 h-40 w-40 translate-x-1/2 translate-y-1/2 rotate-[210deg] transform"
-                    viewBox="0 0 100 100"
-                    fill="rgba(255,255,255,0.03)"
-                >
-                    <rect width="100" height="100" rx="5"></rect>
-                </svg>
-                <div className="bg-noise absolute inset-0 opacity-[0.8]"></div>
-            </div>
             <div
                 className="relative mx-auto max-w-4xl space-y-6 rounded-none bg-gradient-to-r from-orange-400 via-orange-500 to-orange-600 px-4 py-16 text-center shadow-2xl sm:px-6 lg:rounded-lg lg:px-24">
                 <h2 className="text-2xl font-extrabold tracking-tight text-white sm:text-3xl lg:text-4xl">
@@ -86,11 +44,9 @@ export default function CTASection() {
                     <div className="flex flex-col items-center justify-center gap-4 sm:flex-row">
                         <input
                             type="text"
-                            id="cuentaFolio"
                             name="cuentaFolio"
                             placeholder="Introduce tu cuenta-folio"
-                            aria-label="Cuenta-Folio"
-                            className="w-full flex-grow rounded-lg transition-all duration-300 bg-white bg-opacity-80 px-4 py-3 text-gray-800 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-90 sm:w-auto"
+                            className="w-full flex-grow rounded-md transition-all duration-300 bg-white bg-opacity-80 px-4 py-3 text-gray-800 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-90 sm:w-auto"
                             required
                         />
                         <SubmitBtn/>
